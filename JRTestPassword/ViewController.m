e0b0b65eb637c5e619b0818a0e09daf101a6afb4
xiezhenfeng <xiezhenfeng@58ganji.com>
2018-12-20 16:15:57 +0800

    Initial Commit

---
+++ JRTestPassword/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JRTestPassword
+//
+//  Created by 58 on 2018/12/20.
+//  Copyright © 2018年 58. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
